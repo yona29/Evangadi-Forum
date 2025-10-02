c2791f2cc4733e97c40d64b664a2ed2b312a770f
yona29 <yoniyehabtishleg@gmail.com>
2025-10-02 19:32:20 -0400

    sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./App.css";
-
+import SignUp from './Components/SignUp/SignUp';
 const App = () => {
   return (
     <div>
-      
+      <SignUp/>
     </div>
   )
 }
