16c6ca6153b593bbee7ae61574336c9a429b16cb
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-16 04:39:30 -0400

    Remove commented baseURL in axios configuration

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:14255/api",
-  // baseURL: "https://forum-backend-2-ds91.onrender.com//api",
+
 });
 export default instance;
 
