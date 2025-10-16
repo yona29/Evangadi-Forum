b00efbc1339a4832492cf7985ffc12d3979da0e7
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-16 04:48:50 -0400

    Update axios.js

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:14255/api", 
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 export default instance;
 
