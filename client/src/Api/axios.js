4b7e8edc33df2c9972575209d68111712c2f1062
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-22 18:29:01 -0400

    update

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://evangadi-forum-7n4z.onrender.com/api",
+  baseURL: "http://localhost:14255/api",
   headers: {
     "Content-Type": "application/json",
   },
