725e5296b81d5dca8e80d126c9fc20dc67c6c5ed
yona29 <yoniyehabtishleg@gmail.com>
2025-10-28 21:27:06 -0400

    update

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_REMOTE_API_BASE_URL,
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 export default instance;
 
